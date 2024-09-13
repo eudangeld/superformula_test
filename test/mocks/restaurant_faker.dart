2f2c8a4a38defd6cee58dbfd5a1858bd29c621a6
dannylo dangel <eudangeld@gmail.com>
2024-09-13 15:21:02 -0300

    test: faker and mocks

--- test/mocks/restaurant_faker.dart
+++ test/mocks/restaurant_faker.dart
@@ -1,10 +1,20 @@
+import 'package:restaurant_tour/features/restaurants/data/models/restaurant_model.dart';
 import 'package:restaurant_tour/features/restaurants/domain/entities/restaurant.dart';
 import 'package:faker/faker.dart';
 
 List<Restaurant> get fakerListOfRestaurants =>
-    List.generate(20, (_) => fakerSingleRestaurant);
+    List.generate(20, (_) => fakerSingleRestaurantEntity);
 
-Restaurant get fakerSingleRestaurant => Restaurant(
+Restaurant get fakerSingleRestaurantEntity => Restaurant(
+      name: faker.company.name(),
+      price: List.generate(random.integer(4), (_) => '\$').join(),
+      photos: [faker.image.loremPicsum()],
+      rating: random.decimal(min: 1, scale: 5),
+      isCLosed: random.boolean(),
+      address: faker.address.streetName(),
+    );
+
+RestaurantModel get fakerRestaurantModel => RestaurantModel(
       name: faker.company.name(),
       price: List.generate(random.integer(4), (_) => '\$').join(),
       photos: [faker.image.loremPicsum()],
