e057019ea3b3e43aa214eae3d60c3cee93ed0bfd
dannylo dangel <eudangeld@gmail.com>
2024-09-13 10:28:15 -0300

    add: restaurant entitie

---
+++ lib/features/restaurants/domain/entities/restaurant.dart
@@ -0,0 +1,16 @@
+class Restaurant {
+  String name;
+  String price;
+  List<String> photos;
+  double rating;
+  bool isCLosed;
+  String address;
+  Restaurant({
+    required this.name,
+    required this.price,
+    required this.photos,
+    required this.rating,
+    required this.isCLosed,
+    required this.address,
+  });
+}
