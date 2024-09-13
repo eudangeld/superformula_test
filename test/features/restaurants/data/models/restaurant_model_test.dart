87d5f0c9073b9f21d644764b3e357f43e2199a0a
dannylo dangel <eudangeld@gmail.com>
2024-09-13 13:20:41 -0300

    test: smoke test form model

---
+++ test/features/restaurants/data/models/restaurant_model_test.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:restaurant_tour/features/restaurants/data/models/restaurant_model.dart';
+import 'package:restaurant_tour/features/restaurants/domain/entities/restaurant.dart';
+
+import '../../../../fixtures/json_reader.dart';
+
+void main() {
+  /// expected model reading from fixture json
+  final testRestaurantModel = RestaurantModel(
+    name: "Gordon Ramsay Hell's Kitchen",
+    price: "\$\$\$",
+    photos: const [
+      "https://s3-media2.fl.yelpcdn.com/bphoto/q771KjLzI5y638leJsnJnQ/o.jpg",
+    ],
+    rating: 4.4,
+    isCLosed: false,
+    address: "3570 Las Vegas Blvd S",
+  );
+
+  test(
+    'should return a valid model',
+    () async {
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('restaurants_list.json'));
+
+      final result =
+          RestaurantModel.fromJson(jsonMap['data']['search']['business'][0]);
+
+      expect(result, testRestaurantModel);
+      expect(result, isA<Restaurant>());
+    },
+  );
+}
