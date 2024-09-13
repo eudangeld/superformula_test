1cd8fe9d93bc7b9842c799a18aa1721e9f5d926b
dannylo dangel <eudangeld@gmail.com>
2024-09-13 11:33:05 -0300

    add: equatable on entitie restaurant

--- test/features/restaurants/domain/usecases/get_restaurants_list_usecase_test.dart
+++ test/features/restaurants/domain/usecases/get_restaurants_list_usecase_test.dart
@@ -6,6 +6,7 @@ import 'package:restaurant_tour/core/shared_interfaces/usecase.dart';
 import 'package:restaurant_tour/features/restaurants/domain/usecases/get_restaurants_list_usecase.dart';
 
 import '../../../../mocks/mocks_runner.mocks.dart';
+import '../../../../mocks/restaurant_faker.dart';
 
 void main() {
   late GetRestaurantsListUsecase useCase;
@@ -18,9 +19,13 @@ void main() {
   });
 
   test('Should return a list of restaurants', () async {
+    final repoResult = fakerListOfRestaurants;
     when(restaurantsRepoMock.restaurantsList())
-        .thenAnswer((_) => Future.value(const Right([])));
+        .thenAnswer((_) => Future.value(Right(repoResult)));
+
     final result = await useCase(NoParams());
+
     expect(result, isRight);
+    expect(result, isRightOf(repoResult));
   });
 }
