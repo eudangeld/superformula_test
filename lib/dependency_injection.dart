28dcf49821f810b638fb117ee1e75267a565157c
dannylo dangel <eudangeld@gmail.com>
2024-09-13 22:24:51 -0300

    add: graphql client done : )

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,18 +1,23 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:restaurant_tour/features/restaurants/presentation/bloc/restaurants_bloc.dart';
 
 import 'features/restaurants/data/data_sources/restaurant_remote_data_source_implementation.dart';
 import 'features/restaurants/data/data_sources/restaurants_remote_data_source.dart';
 import 'features/restaurants/data/repositories/restaurants_repository_impl.dart';
 import 'features/restaurants/domain/repositories/restaurants_repository.dart';
 import 'features/restaurants/domain/usecases/get_restaurants_list_usecase.dart';
+import 'features/restaurants/presentation/bloc/restaurants_bloc.dart';
 
 final serviceLocator = GetIt.instance;
 
 void injectDependencies() {
-  final HttpLink graphqlLink = HttpLink(
-    'https://api.github.com/graphql',
+  final graphqlUrl = dotenv.get('BASE_URL');
+  final apiKey = dotenv.get('BASE_URL');
+
+  final graphqlLink = HttpLink(
+    graphqlUrl,
+    defaultHeaders: {'Authorization': 'Bearer $apiKey'},
   );
 
   final GraphQLClient graphQLClient =
