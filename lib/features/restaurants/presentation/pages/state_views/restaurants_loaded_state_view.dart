ab74bda83888ae46bcb40c994db48ba34f3da85b
dannylo dangel <eudangeld@gmail.com>
2024-09-13 23:15:10 -0300

    add: rendering loaded state

--- lib/features/restaurants/presentation/pages/state_views/restaurants_loaded_state_view.dart
+++ lib/features/restaurants/presentation/pages/state_views/restaurants_loaded_state_view.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_tour/features/restaurants/presentation/bloc/restaurants_state.dart';
+
+import '../widgets/restaurant_tile_widget.dart';
 
 class LoadedStateView extends StatelessWidget {
-  const LoadedStateView({super.key});
+  final LoadedState state;
+
+  const LoadedStateView(this.state, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Loaded'),
-    );
+    return ListView.builder(
+        itemCount: state.restaurants.length,
+        itemBuilder: (_, index) =>
+            RestaurantTileWidget(state.restaurants[index]));
   }
 }
