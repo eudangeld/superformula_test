7523ffbcebd805da70a5e966831fb2e5c4872dcb
dannylo dangel <eudangeld@gmail.com>
2024-09-13 21:36:39 -0300

    refact: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:restaurant_tour/models/restaurant.dart';
-import 'package:restaurant_tour/query.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-const _apiKey =
-    '2VDlHa48CUC4UXbEKYUtcaRdOuXosr2xmou6dWcDRzXiqOlYUyoziH856e9mBGrSfZ_C7wwoIt0-LfC7xi-dMLSx9aguS47t6Jf-W4ddYZekNtMSBE5AsuQHABDpYnYx';
-const _baseUrl = 'https://api.yelp.com/v3/graphql';
+import './dependency_injection.dart' as di;
+import 'dependency_injection.dart';
+import 'features/restaurants/presentation/bloc/restaurants_bloc.dart';
+import 'features/restaurants/presentation/pages/restaurants_page.dart';
 
-void main() {
+main() async {
+  di.injectDependencies();
   runApp(const RestaurantTour());
 }
 
@@ -18,70 +16,13 @@ class RestaurantTour extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Restaurant Tour',
-      home: HomePage(),
-    );
-  }
-}
-
-// TODO: Architect code
-// This is just a POC of the API integration
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  Future<RestaurantQueryResult?> getRestaurants({int offset = 0}) async {
-    final headers = {
-      'Authorization': 'Bearer $_apiKey',
-      'Content-Type': 'application/graphql',
-    };
-
-    try {
-      final response = await http.post(
-        Uri.parse(_baseUrl),
-        headers: headers,
-        body: query(offset),
-      );
-
-      if (response.statusCode == 200) {
-        return RestaurantQueryResult.fromJson(
-          jsonDecode(response.body)['data']['search'],
-        );
-      } else {
-        print('Failed to load restaurants: ${response.statusCode}');
-        return null;
-      }
-    } catch (e) {
-      print('Error fetching restaurants: $e');
-      return null;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Restaurant Tour'),
-            ElevatedButton(
-              child: const Text('Fetch Restaurants'),
-              onPressed: () async {
-                try {
-                  final result = await getRestaurants();
-                  if (result != null) {
-                    print('Fetched ${result.restaurants!.length} restaurants');
-                  } else {
-                    print('No restaurants fetched');
-                  }
-                } catch (e) {
-                  print('Failed to fetch restaurants: $e');
-                }
-              },
-            ),
-          ],
-        ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => serviceLocator<RestaurantsBloc>()),
+      ],
+      child: const MaterialApp(
+        title: 'Restaurant Tour',
+        home: RestaurantsPage(),
       ),
     );
   }
