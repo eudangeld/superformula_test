028058fc1212198bd06e3b7f668976ca107da862
dannylo dangel <eudangeld@gmail.com>
2024-09-16 09:04:28 -0300

    add: open close widget

---
+++ lib/features/restaurants/presentation/pages/widgets/open_closed_indicator_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_tour/features/restaurants/presentation/pages/strings/en_strings.dart';
+
+import '../../../../../typography.dart';
+
+class OpenClosedIndicatorWidget extends StatelessWidget {
+  const OpenClosedIndicatorWidget({super.key, required this.value});
+
+  final bool value;
+
+  final openGreen = const Color(0xff5CD313);
+  final closedRed = const Color(0xffEA5E5E);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          value ? EnRestaurantsStrings.openNow : EnRestaurantsStrings.closed,
+          style: AppTextStyles.openRegularItalic,
+        ),
+        const SizedBox(width: 10),
+        Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle, color: value ? openGreen : closedRed),
+        ),
+      ],
+    );
+  }
+}
