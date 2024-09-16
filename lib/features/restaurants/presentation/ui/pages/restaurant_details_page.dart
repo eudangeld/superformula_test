e0d7a21f22a2ffeea3f481029a71969805d9a209
dannylo dangel <eudangeld@gmail.com>
2024-09-16 14:14:58 -0300

    add: handle state - handle backbutton

--- lib/features/restaurants/presentation/ui/pages/restaurant_details_page.dart
+++ lib/features/restaurants/presentation/ui/pages/restaurant_details_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurant_tour/features/restaurants/presentation/bloc/restaurants_bloc.dart';
+import 'package:restaurant_tour/features/restaurants/presentation/bloc/restaurants_events.dart';
+import 'package:restaurant_tour/features/restaurants/presentation/bloc/restaurants_state.dart';
+
+import '../state_views/restaurants_loading_state_view.dart';
 
 class RestaurantDetailsPage extends StatefulWidget {
   const RestaurantDetailsPage({super.key});
@@ -9,11 +15,30 @@ class RestaurantDetailsPage extends StatefulWidget {
 
 class _RestaurantStateDetailsPage extends State<RestaurantDetailsPage> {
   String appBarTitle = '';
+  _handleBackButton(context) {
+    BlocProvider.of<RestaurantsBloc>(context).add(GetRestaurantsListEvent());
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return PopScope(
+      canPop: true,
+      onPopInvoked: (bool didPop) async {
+        _handleBackButton(context);
+      },
+      child: Scaffold(
+        appBar: AppBar(),
+        body: BlocBuilder<RestaurantsBloc, RestaurantState>(
+          builder: (_, state) {
+            return switch (state) {
+              LoadingState() => const LoadingStateView(),
+              ErrorState() => const Text('error'),
+              RestaurantDetailsLoaded() => const Text('DETAILS LOADED'),
+              _ => const SizedBox.shrink(),
+            };
+          },
+        ),
+      ),
     );
   }
 }
