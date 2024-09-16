db58df83da7f45736aec0fa02236f6a42d4fad6d
dannylo dangel <eudangeld@gmail.com>
2024-09-16 19:16:48 -0300

    add: favorites data source

---
+++ lib/features/restaurants/data/data_sources/favorites_local_data_source.dart
@@ -0,0 +1,7 @@
+import 'package:restaurant_tour/features/restaurants/data/models/restaurant_model.dart';
+
+abstract class FavoritesLocalDataSource {
+  Future<List<RestaurantModel>> list();
+  Future<void> save(RestaurantModel restaurant);
+  Future<void> delete(String id);
+}
---
+++ lib/features/restaurants/data/data_sources/favorites_local_data_source_implementation.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/restaurant_model.dart';
+import 'favorites_local_data_source.dart';
+
+class FavoritesLocalDataSourceImpl implements FavoritesLocalDataSource {
+  final SharedPreferences localStorage;
+
+  FavoritesLocalDataSourceImpl({required this.localStorage});
+
+  final String favoritesKey = 'favorites';
+
+  @override
+  Future<void> delete(String id) async {
+    await localStorage.remove('${favoritesKey}_$id');
+  }
+
+  @override
+  Future<List<RestaurantModel>> list() async {
+    final storageKeys = localStorage.getKeys();
+    final List<RestaurantModel> response = [];
+
+    for (final key in storageKeys) {
+      if (key.startsWith(favoritesKey)) {
+        final modelString = localStorage.getString(key);
+        final model = RestaurantModel.fromJson(jsonDecode(modelString!));
+        response.add(model);
+      }
+    }
+    return response;
+  }
+
+  @override
+  Future<void> save(RestaurantModel restaurant) async {
+    final restaurantString = jsonEncode(restaurant.toJson());
+    localStorage.setString(
+        '${favoritesKey}_${restaurant.id}', restaurantString);
+  }
+}
