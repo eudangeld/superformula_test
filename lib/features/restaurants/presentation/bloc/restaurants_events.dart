1eed408a6369b446ea71cd90137362bc3016894a
dannylo dangel <eudangeld@gmail.com>
2024-09-16 19:18:32 -0300

    add: favorite bloc event

--- lib/features/restaurants/presentation/bloc/restaurants_events.dart
+++ lib/features/restaurants/presentation/bloc/restaurants_events.dart
@@ -1,6 +1,14 @@
+import 'package:restaurant_tour/features/restaurants/domain/entities/restaurant_details.dart';
+
 abstract class RestaurantEvent {}
 
 class GetRestaurantsListEvent extends RestaurantEvent {}
+
+class SaveFavoriteEvent extends RestaurantEvent {
+  final RestaurantDetails restaurant;
+
+  SaveFavoriteEvent({required this.restaurant});
+}
 
 class GetRestaurantDetailsEvent extends RestaurantEvent {
   final String restaurantId;
