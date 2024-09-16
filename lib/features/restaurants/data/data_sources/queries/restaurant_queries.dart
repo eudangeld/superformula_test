b8d096c1c9a2af477438cb44fed3c36b0424c5b8
dannylo dangel <eudangeld@gmail.com>
2024-09-16 13:53:41 -0300

    chore: lint import

--- lib/features/restaurants/data/data_sources/queries/restaurant_queries.dart
+++ lib/features/restaurants/data/data_sources/queries/restaurant_queries.dart
@@ -17,3 +17,30 @@ const String restaurantListQuery = '''
     }
   }
 }''';
+const String restaurantDetailsQuery = '''
+  query {
+  business(id: "2iTsRqUsPGRH1li1WVRvKQ") {
+    name
+    rating
+    price
+    is_closed
+    photos
+     categories {
+        title
+      }
+    reviews {
+      rating
+      text
+      user {
+        name
+        image_url
+      }
+    }
+    location {
+      address1
+      state
+      city
+      postal_code
+    }
+  }
+}''';
