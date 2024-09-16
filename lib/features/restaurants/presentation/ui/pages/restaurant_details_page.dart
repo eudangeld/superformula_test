d3493eeb6c6ddd56e115cebf8575727712ae1945
dannylo dangel <eudangeld@gmail.com>
2024-09-16 16:02:19 -0300

    add: always show back button

--- lib/features/restaurants/presentation/ui/pages/restaurant_details_page.dart
+++ lib/features/restaurants/presentation/ui/pages/restaurant_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurant_tour/typography.dart';
 
 import '../../bloc/restaurants_bloc.dart';
 import '../../bloc/restaurants_events.dart';
@@ -7,15 +8,9 @@ import '../../bloc/restaurants_state.dart';
 import '../state_views/restaurant_details_state_view.dart';
 import '../state_views/restaurants_loading_state_view.dart';
 
-class RestaurantDetailsPage extends StatefulWidget {
+class RestaurantDetailsPage extends StatelessWidget {
   const RestaurantDetailsPage({super.key});
 
-  @override
-  State<RestaurantDetailsPage> createState() => _RestaurantStateDetailsPage();
-}
-
-class _RestaurantStateDetailsPage extends State<RestaurantDetailsPage> {
-  String appBarTitle = '';
   _handleBackButton(context) {
     BlocProvider.of<RestaurantsBloc>(context).add(GetRestaurantsListEvent());
   }
@@ -27,19 +22,26 @@ class _RestaurantStateDetailsPage extends State<RestaurantDetailsPage> {
       onPopInvoked: (bool didPop) async {
         _handleBackButton(context);
       },
-      child: Scaffold(
-        appBar: AppBar(),
-        body: BlocBuilder<RestaurantsBloc, RestaurantState>(
-          builder: (_, state) {
-            return switch (state) {
-              LoadingState() => const LoadingStateView(),
-              RestaurantDetailsLoaded() => RestaurantDetailsStateView(
+      child: BlocBuilder<RestaurantsBloc, RestaurantState>(
+        builder: (_, state) {
+          return switch (state) {
+            LoadingState() =>
+              Scaffold(body: const LoadingStateView(), appBar: AppBar()),
+            RestaurantDetailsLoaded() => Scaffold(
+                appBar: AppBar(
+                  title: Text(
+                    state.restaurantDetails.name,
+                    style: AppTextStyles.loraRegularHeadline,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                body: RestaurantDetailsStateView(
                   state.restaurantDetails,
                 ),
-              _ => const SizedBox.shrink(),
-            };
-          },
-        ),
+              ),
+            _ => const SizedBox.shrink(),
+          };
+        },
       ),
     );
   }
