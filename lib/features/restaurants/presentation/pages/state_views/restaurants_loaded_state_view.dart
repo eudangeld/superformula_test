53728fcbff6a16f73a4f520904c655bc4c9cba05
dannylo dangel <eudangeld@gmail.com>
2024-09-16 10:57:21 -0300

    chore: lint

--- lib/features/restaurants/presentation/pages/state_views/restaurants_loaded_state_view.dart
+++ lib/features/restaurants/presentation/pages/state_views/restaurants_loaded_state_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_tour/features/restaurants/presentation/bloc/restaurants_state.dart';
 
+import '../../bloc/restaurants_state.dart';
 import '../widgets/restaurant_tile_widget.dart';
 
 class LoadedStateView extends StatelessWidget {
