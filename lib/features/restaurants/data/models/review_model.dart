e136f134c7db289735a5fdb536535e6290d0b157
dannylo dangel <eudangeld@gmail.com>
2024-09-16 13:32:18 -0300

    add: review entitie

---
+++ lib/features/restaurants/data/models/review_model.dart
@@ -0,0 +1,19 @@
+import '../../domain/entities/review.dart';
+
+class ReviewModel extends Review {
+  ReviewModel({
+    required super.rating,
+    required super.text,
+    required super.userName,
+    required super.photoUrl,
+  });
+
+  factory ReviewModel.fromJson(dynamic data) {
+    return ReviewModel(
+      rating: data['rating'],
+      text: data['text'],
+      userName: data['user']['name'],
+      photoUrl: data['user']['image_url'],
+    );
+  }
+}
---
+++ lib/features/restaurants/domain/entities/review.dart
@@ -0,0 +1,13 @@
+class Review {
+  final double rating;
+  final double text;
+  final double userName;
+  final double photoUrl;
+
+  Review({
+    required this.rating,
+    required this.text,
+    required this.userName,
+    required this.photoUrl,
+  });
+}
