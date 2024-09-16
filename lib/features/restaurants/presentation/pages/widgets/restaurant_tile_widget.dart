67669f03862d1eb3fca917d78a2a3449f67c8732
dannylo dangel <eudangeld@gmail.com>
2024-09-16 09:04:45 -0300

    add: restaurant tile styles

--- lib/features/restaurants/presentation/pages/widgets/restaurant_tile_widget.dart
+++ lib/features/restaurants/presentation/pages/widgets/restaurant_tile_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_tour/features/restaurants/domain/entities/restaurant.dart';
+import 'package:restaurant_tour/features/restaurants/presentation/pages/widgets/open_closed_indicator_widget.dart';
+import 'package:restaurant_tour/features/restaurants/presentation/pages/widgets/rating_stars.dart';
+
+import '../../../../../typography.dart';
 
 class RestaurantTileWidget extends StatelessWidget {
   final Restaurant restaurantEntitie;
@@ -9,36 +13,98 @@ class RestaurantTileWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const double aspect = 100;
-    return Row(
-      children: [
-        Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-          child: Image.network(
-            restaurantEntitie.photos.first,
-            height: aspect,
-            width: aspect,
-            fit: BoxFit.cover,
-            loadingBuilder: (BuildContext context, Widget child,
-                ImageChunkEvent? loadingProgress) {
-              if (loadingProgress == null) return child;
-              return Center(
-                child: CircularProgressIndicator(
-                  value: loadingProgress.expectedTotalBytes != null
-                      ? loadingProgress.cumulativeBytesLoaded /
-                          loadingProgress.expectedTotalBytes!
-                      : null,
-                ),
-              );
-            },
+    final size = MediaQuery.of(context).size;
+    return Card(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                restaurantEntitie.photos.first,
+                height: aspect,
+                width: aspect,
+                fit: BoxFit.cover,
+                loadingBuilder: (
+                  BuildContext context,
+                  Widget child,
+                  ImageChunkEvent? loadingProgress,
+                ) {
+                  if (loadingProgress == null) return child;
+                  return Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded /
+                              loadingProgress.expectedTotalBytes!
+                          : null,
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
-        ),
-        Column(
-          children: [
-            Text(restaurantEntitie.name),
-            Text(restaurantEntitie.price),
-          ],
-        )
-      ],
+          SizedBox(
+            height: size.height * .14,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: SizedBox(
+                    width: size.width * .55,
+                    child: Text(
+                      restaurantEntitie.name,
+                      style: AppTextStyles.loraRegularTitle,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: size.width * .55,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              restaurantEntitie.price,
+                              style: AppTextStyles.openRegularTitle,
+                            ),
+                            const SizedBox(height: 5),
+                            RatingStarsWidget(
+                              value: restaurantEntitie.rating,
+                            )
+                          ],
+                        ),
+                        OpenClosedIndicatorWidget(
+                          value: restaurantEntitie.isCLosed,
+                        )
+                        // Text('JOSE')
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
+
+final boxShadow = [
+  BoxShadow(
+    color: Colors.grey.withOpacity(0.3),
+    spreadRadius: 2,
+    blurRadius: 3,
+    offset: const Offset(5, 5), // changes position of shadow
+  ),
+];
