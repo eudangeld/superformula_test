a1d94ee9371d1824055126c428bcc35990f9955d
dannylo dangel <eudangeld@gmail.com>
2024-09-16 11:40:57 -0300

    add: add navigation handler

--- lib/features/restaurants/presentation/ui/widgets/restaurant_tile_widget.dart
+++ lib/features/restaurants/presentation/ui/widgets/restaurant_tile_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_tour/core/route/app_routes.dart';
 import 'package:restaurant_tour/features/restaurants/domain/entities/restaurant.dart';
 import 'package:restaurant_tour/features/restaurants/presentation/ui/widgets/open_closed_indicator_widget.dart';
 import 'package:restaurant_tour/features/restaurants/presentation/ui/widgets/rating_stars.dart';
@@ -14,98 +15,102 @@ class RestaurantTileWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     const double aspect = 100;
     final size = MediaQuery.of(context).size;
-    return Card(
-      color: Colors.white,
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                restaurantEntitie.photos.first,
-                height: aspect,
-                width: aspect,
-                fit: BoxFit.cover,
-                loadingBuilder: (
-                  BuildContext context,
-                  Widget child,
-                  ImageChunkEvent? loadingProgress,
-                ) {
-                  if (loadingProgress == null) return child;
-                  return Center(
-                    child: CircularProgressIndicator(
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded /
-                              loadingProgress.expectedTotalBytes!
-                          : null,
-                    ),
-                  );
-                },
+    return InkWell(
+      onTap: () =>
+          Navigator.of(context).pushNamed(AppRoutes.RESTAURANT_DETAILS),
+      child: Card(
+        color: Colors.white,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  restaurantEntitie.photos.first,
+                  height: aspect,
+                  width: aspect,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (
+                    BuildContext context,
+                    Widget child,
+                    ImageChunkEvent? loadingProgress,
+                  ) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes!
+                            : null,
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-          const SizedBox(width: 5),
-          SizedBox(
-            height: size.height * .14,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: SizedBox(
-                    width: size.width * .55,
-                    child: Text(
-                      restaurantEntitie.name,
-                      style: AppTextStyles.loraRegularTitle,
+            const SizedBox(width: 5),
+            SizedBox(
+              height: size.height * .14,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: SizedBox(
+                      width: size.width * .55,
+                      child: Text(
+                        restaurantEntitie.name,
+                        style: AppTextStyles.loraRegularTitle,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: size.width * .60,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  restaurantEntitie.price,
-                                  style: AppTextStyles.openRegularText,
-                                ),
-                                const SizedBox(width: 5),
-                                Text(
-                                  restaurantEntitie.categories.first,
-                                  style: AppTextStyles.openRegularText,
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 5),
-                            RatingStarsWidget(
-                              value: restaurantEntitie.rating,
-                            )
-                          ],
-                        ),
-                        OpenClosedIndicatorWidget(
-                          value: restaurantEntitie.isCLosed,
-                        )
-                        // Text('JOSE')
-                      ],
+                  SizedBox(
+                    width: size.width * .60,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    restaurantEntitie.price,
+                                    style: AppTextStyles.openRegularText,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    restaurantEntitie.categories.first,
+                                    style: AppTextStyles.openRegularText,
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 5),
+                              RatingStarsWidget(
+                                value: restaurantEntitie.rating,
+                              )
+                            ],
+                          ),
+                          OpenClosedIndicatorWidget(
+                            value: restaurantEntitie.isCLosed,
+                          )
+                          // Text('JOSE')
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
