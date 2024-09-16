dd0165846b03d24fdac570de0e4bd7636ae2d9cb
dannylo dangel <eudangeld@gmail.com>
2024-09-16 11:33:14 -0300

    add: strings

--- lib/features/restaurants/presentation/pages/strings/en_strings.dart
+++ lib/features/restaurants/presentation/pages/strings/en_strings.dart
@@ -1,4 +1,7 @@
 class EnRestaurantsStrings {
+  static String appName = 'RestauranTour';
   static String openNow = 'Open now';
   static String closed = 'Closed';
+  static String allRestaurantsTabTitle = 'All Restaurants';
+  static String favoritesTabTitle = 'My Favorites';
 }
