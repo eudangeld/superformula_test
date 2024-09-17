c3e72addaa52981881e038cbe6a35189027455e7
dannylo dangel <eudangeld@gmail.com>
2024-09-17 11:03:52 -0300

    add: inject all favorite usecase

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,18 +1,21 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:restaurant_tour/features/restaurants/data/repositories/favorites_repository_impl.dart';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'features/restaurants/data/data_sources/favorites_local_data_source.dart';
 import 'features/restaurants/data/data_sources/favorites_local_data_source_implementation.dart';
 import 'features/restaurants/data/data_sources/restaurant_remote_data_source_implementation.dart';
 import 'features/restaurants/data/data_sources/restaurants_remote_data_source.dart';
+import 'features/restaurants/data/repositories/favorites_repository_impl.dart';
 import 'features/restaurants/data/repositories/restaurants_repository_impl.dart';
 import 'features/restaurants/domain/repositories/favorites_repository.dart';
 import 'features/restaurants/domain/repositories/restaurants_repository.dart';
+import 'features/restaurants/domain/usecases/delete_favorites_usecase.dart';
 import 'features/restaurants/domain/usecases/get_restaurant_details_usecase.dart';
 import 'features/restaurants/domain/usecases/get_restaurants_list_usecase.dart';
+import 'features/restaurants/domain/usecases/list_favorites_usecase.dart';
 import 'features/restaurants/domain/usecases/save_favorite_usecase.dart';
 import 'features/restaurants/presentation/bloc/restaurants_bloc.dart';
 
@@ -38,6 +41,10 @@ Future<void> injectDependencies() async {
       FavoritesRepositoryImpl(localFavoritesDataSource: serviceLocator()));
   serviceLocator.registerLazySingleton(
       () => SaveFavoriteUsecase(favoritesRepository: serviceLocator()));
+  serviceLocator.registerLazySingleton(
+      () => ListFavoritesUsecase(favoritesRepository: serviceLocator()));
+  serviceLocator.registerLazySingleton(
+      () => DeleteFavoritesUsecase(favoritesRepository: serviceLocator()));
 
   serviceLocator.registerLazySingleton<RestaurantsRemoteDataSource>(() =>
       RestaurantsRemoteSourceImplementation(graphQlClient: graphQLClient));
@@ -45,11 +52,20 @@ Future<void> injectDependencies() async {
       () => RestaurantsRepositoryImpl(remoteDataSource: serviceLocator()));
   serviceLocator.registerLazySingleton(
       () => GetRestaurantsListUsecase(restaurantsRepository: serviceLocator()));
-  serviceLocator.registerLazySingleton(() =>
-      GetRestaurantDetailsUsecase(restaurantsRepository: serviceLocator()));
+  serviceLocator.registerLazySingleton(
+    () => GetRestaurantDetailsUsecase(
+      restaurantsRepository: serviceLocator(),
+      favoritesRepository: serviceLocator(),
+    ),
+  );
 
-  serviceLocator.registerLazySingleton(() => RestaurantsBloc(
+  serviceLocator.registerLazySingleton(
+    () => RestaurantsBloc(
       getRestaurantsUseCase: serviceLocator(),
       getRestaurantDetailsUsecase: serviceLocator(),
-      saveFavoriteUsecase: serviceLocator()));
+      saveFavoriteUsecase: serviceLocator(),
+      listFavoritesUsecase: serviceLocator(),
+      deleteFavoritesUsecase: serviceLocator(),
+    ),
+  );
 }
